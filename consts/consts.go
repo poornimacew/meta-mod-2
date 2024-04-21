db13b0dd46f7accc792916bc7b6c7bf19e8a50d8
poornimacew <167696179+poornimacew@users.noreply.github.com>
2024-04-21 21:27:17 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "pooran"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "par"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "p"
 )
 
 var ID ids.ID
